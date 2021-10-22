aec1c5971b325f9e66f81e056c3ff7f3bca69d0a
ibqn <evgen.ibqn@gmail.com>
2021-10-22 21:46:28 +0200

    improve paragraph

--- src/components/paragraph.jsx
+++ src/components/paragraph.jsx
@@ -6,17 +6,17 @@ const P = styled.p`
 
 const P1 = styled(P)`
   font-size: 18px;
-  line-height: 29px;
+  line-height: 1.6;
 `
 
 const P2 = styled(P)`
   font-size: 16px;
-  line-height: 24px;
+  line-height: 1.5;
 `
 
 const P3 = styled(P)`
   font-size: 14px;
-  line-height: 24px;
+  line-height: 1.7;
 `
 
 export { P1, P2, P3 }
