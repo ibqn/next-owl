ececeef44ce71813b6615b2d7b87ceda6a9c2b62
ibqn <evgen.ibqn@gmail.com>
2021-10-22 21:37:13 +0200

    add logo to sidebar

--- src/components/sidebar.jsx
+++ src/components/sidebar.jsx
@@ -1,15 +1,22 @@
 import styled from 'styled-components'
+import Logo from 'components/logo'
 
 const Div = styled.div`
   grid-area: sidebar;
+
+  display: grid;
+  align-content: start;
+  gap: 20px;
 
   @media (max-width: 765px) {
     display: none;
   }
 `
 
-const Sidebar = () => {
-  return <Div>sidebar</Div>
-}
+const Sidebar = () => (
+  <Div>
+    <Logo />
+  </Div>
+)
 
 export default Sidebar
