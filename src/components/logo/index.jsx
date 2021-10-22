08c473a3e9226b84d38708f6276d6efee4862586
ibqn <evgen.ibqn@gmail.com>
2021-10-22 18:59:10 +0200

    add logo component

---
+++ src/components/logo/index.jsx
@@ -0,0 +1,11 @@
+import styled from 'styled-components'
+import LogoIcon from './logo.svg'
+
+// const StyledLogo = styled('div')`
+const StyledLogo = styled(LogoIcon)`
+  width: 159px;
+  height: 44px;
+  fill: #7653fc;
+`
+
+export default StyledLogo
